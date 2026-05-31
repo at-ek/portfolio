c7f1fb5ebef037cc37642cbd84374130738a3dea
at-ek <keita.shida0806p@gmail.com>
2026-05-31 23:04:19 +0900

    update

--- src/data/works.js
+++ src/data/works.js
@@ -1,6 +1,7 @@
 export const works = [
     {id: 1, name: 'ポートフォリオ', description: 'Reactを使用してポートフォリオを製作しました。\nuseStateやJSでの配列操作の復習を兼ねての制作になります。\n仕様やデータ設計、エラーなどChatGPTの力を借り進めました。ReactやJSだけでなくHTMLのタグの使い分けなど、以前学習した時よりも多くの学びがあったため今後意識すべきものを理解することもできました。', src: 'portfolio_portfolio.jpg', link: '#'},
     {id: 2, name: 'ToDoアプリ', description: 'Reactを使用してTodoアプリを製作しました。\n機能のロジックを考え、どのようにコードに落とし込むかを課題として取り組みました。その上でどこまでChatGPTに任せ、どこまで自力でやるかを考えて実装しました。', src: 'todo_portfolio.jpg', link: 'https://todo-xi-ten-15.vercel.app/'},
+    {id: 3, name: 'ECサイト', description: 'Reactを使用してECサイトを製作しました。\nuseStateをどう管理し、なにを関数化するかを考えながら制作しました。今回もChatGPTに頼りながら進めましたが、後半は自力で実装できる機能もあり自信にもつながりました。', src: 'ec_portfolio', link: 'https://ec-omega-two.vercel.app/'},
 ]
 
 export const worksDami = [
